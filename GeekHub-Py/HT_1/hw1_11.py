bbb8619eba7b4c59b428b2dd6f5c09084a7e9dba
OAntonenko <oantonenko@healthprecision.com>
2020-11-10 12:27:04 +0200

    removed useless variable

--- GeekHub-Py/HT_1/hw1_11.py
+++ GeekHub-Py/HT_1/hw1_11.py
@@ -4,12 +4,10 @@ Write a script to remove duplicates from Dictionary.
 """
 
 test_dict = {1:10, 2:20, 3:20, 4:40}
-found_values = []
 dict_with_unique_val = {}
 
 for k, v in test_dict.items():
-	if v not in found_values:
+	if v not in dict_with_unique_val.values():
 		dict_with_unique_val[k] = v
-		found_values.append(v)
 
 print(dict_with_unique_val)
