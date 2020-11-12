d9d00c7affd6f98d6c66acfd2013b1df1e922f77
OAntonenko <oantonenko@healthprecision.com>
2020-11-12 12:51:59 +0200

    homework 2 task 3

---
+++ GeekHub-Py/HT_2/hw2_3.py
@@ -0,0 +1,20 @@
+"""
+*** Task 3 ***
+Написати функцiю season, яка приймає один аргумент — номер мiсяця (вiд 1 до 12), яка буде повертати пору року, 
+якiй цей мiсяць належить (зима, весна, лiто або осiнь)
+"""
+
+def season(month: int):
+	month_to_season = {
+		(12, 1, 2): "winter",
+		(3, 4, 5): "spring",
+		(6, 7, 8): "summer",
+		(9, 10, 11): "autumn"
+	}
+	for k, v in month_to_season.items():
+		if month in k:
+			return v
+	return "Not corrent number of month"
+
+
+print(season(int(input("Number of month: "))))
