0656a8ead1c31405fd5c41e4785628e2467332ac
OAntonenko <oantonenko@healthprecision.com>
2020-11-15 10:20:57 +0200

    homework 3 task 3

---
+++ GeekHub-Py/HT_3/hw3_3.py
@@ -0,0 +1,17 @@
+"""
+*** Task 3 ***
+Написати функцию < is_prime >, яка прийматиме 1 аргумент - число від 0 до 1000, и яка вертатиме True, якщо це число просте, и False - якщо ні.
+"""
+
+def is_prime(n):
+	if n < 2:
+		return False
+
+	for i in range(2, n):
+		if n % i == 0:
+			return False
+
+	return True
+
+
+print(is_prime(17))
